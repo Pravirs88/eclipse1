c90528d1d55c9546e4f0529c93ea94d905f35ab7
PravirS <salunkhe.pravir88@gmail.com>
2022-11-01 19:37:28 +0530

    changes
    hiiii

--- src/selenium/Zomato.java
+++ src/selenium/Zomato.java
@@ -14,7 +14,7 @@ public class Zomato {
 		sc.findElement(By.xpath("/html/body/div/div/div/div/div/div/div/div/div/form/div/div/input")).sendKeys("salunkhe.pravir88@gmail.com");//absolute xpath
 		sc.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[1]/div[2]/div/input")).sendKeys("prav1234");
 		sc.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).submit();
-		
+		System.out.println("hhhh");
 	}
 
 }
